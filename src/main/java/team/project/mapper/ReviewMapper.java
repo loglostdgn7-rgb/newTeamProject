2ba19069ff634aa0822c50f00b5843448c897496
Fnvit <fnvit@naver.com>
2025-09-29 21:39:54 +0900

    Init
    CSS 수정 중

--- src/main/java/team/project/mapper/ReviewMapper.java
+++ src/main/java/team/project/mapper/ReviewMapper.java
@@ -1,17 +1,24 @@
 package team.project.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import team.project.dto.PagenationDTO;
 import team.project.dto.ReviewDTO;
+import team.project.dto.UserDTO;
+
 import java.util.List;
 
 @Mapper
 public interface ReviewMapper {
 
-    // 리뷰 리스트 조회
-    List<ReviewDTO> getReviewList(PagenationDTO reviewDTO);
+    // 유저가 작성한 리뷰 리스트 조회
+    List<ReviewDTO> selectUserReviews(
+            @Param("pagenation") PagenationDTO<ReviewDTO> pagenation,
+            @Param("user") UserDTO user
+    );
 
-    // 리뷰 총 개수 조회
-    int getReviewCount(PagenationDTO reviewDTO);
+    // 유저가 작성한 리뷰 총 개수 조회
+    int selectReviewCount(UserDTO user);
+
 
 }
--- src/main/java/team/project/service/ReviewService.java
+++ src/main/java/team/project/service/ReviewService.java
@@ -3,7 +3,9 @@ package team.project.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.project.dto.PagenationDTO;
+import team.project.dto.ProductDTO;
 import team.project.dto.ReviewDTO;
+import team.project.dto.UserDTO;
 import team.project.mapper.ReviewMapper;
 
 import java.util.List;
@@ -11,21 +13,21 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
-
     private final ReviewMapper reviewMapper;
 
-    public PagenationDTO getReviews(PagenationDTO<ReviewDTO> reviewDTO) {
-
-        // 전체 리뷰 수 가져오기
-        int totalCount = reviewMapper.getReviewCount(reviewDTO);
-        reviewDTO.setTotalElementsCount(totalCount); // 총 페이지/인덱스 계산 포함
-
-        // 리뷰 리스트 가져오기
-        List<ReviewDTO> reviews = reviewMapper.getReviewList(reviewDTO);
-//        reviewDTO.setReviews(reviews); // reviews 필드에 세팅
-        reviewDTO.setElements(reviews); // reviews 필드에 세팅
-
-
-        return reviewDTO;
+    public void getUserReviews(PagenationDTO<ReviewDTO> pagenation, UserDTO user) {
+        List<ReviewDTO> elements = reviewMapper.selectUserReviews(pagenation, user);
+        // 화면에 표시할 요소가 있다면
+        if(elements != null && !elements.isEmpty()) {
+            Integer totalElementCount = reviewMapper.selectReviewCount(user);
+            pagenation.setTotalElementsCount(totalElementCount);
+            // 페이지네이션 객체가 실제로 화면에 표시할 데이터들을 가지게 한다
+            pagenation.setElements(elements);
+        }
+        // 화면에 표시할 요소가 없다면
+        else{
+            pagenation.setTotalElementsCount(0);
+            pagenation.setElements(List.of());
+        }
     }
 }
